ebcf0c1f9fd4de3dd5fd470f91a04ab241bcf03e
eZice <easydryice@gmail.com>
2024-12-03 03:17:34 +0530

    fixing cart system

--- src/context/CartContextProvider.jsx
+++ src/context/CartContextProvider.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import CartContext from './CartContext'
 
 const CartContextProvider = ({children}) => {
     const [totalCount , setTotalCount] = useState (0)
+
+    const [data ,setData] = useState([])
+
+
+
+
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error('API failed')
+            }
+            return res.json();
+          })
+          .then((res) => setData(res.map((product) => ({...product , count: 0}))))
+          .catch((error) => console.error('Error fetching data:', error))
+      }, [])
+
+
     return(
-        <CartContext.Provider value={{totalCount , setTotalCount}}>
+        <CartContext.Provider value={{totalCount , setTotalCount , data , setData}}>
             {children}
         </CartContext.Provider >
 
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,33 +3,35 @@ import CartContext from '../context/CartContext'
 
 function Product() {
 
-    const [data ,setData] = useState([])
-    const [cartCount , setCartCount ] = useState(0)
+    // const [data ,setData] = useState([])
 
     const {setTotalCount} = useContext(CartContext)
+    const {totalCount}  = useContext(CartContext)
+    const {data}  = useContext(CartContext)
+    const {setData}  = useContext(CartContext)
 
 
 
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-          .then((res) => {
-            if (!res.ok) {
-              throw new Error('API failed')
-            }
-            return res.json();
-          })
-          .then((res) => setData(res.map((product) => ({...product , count: 0}))))
-          .catch((error) => console.error('Error fetching data:', error))
-      }, [])
 
-    useEffect(()=>{
-      setTotalCount(cartCount)
-    },[cartCount])
+    // useEffect(() => {
+    //     fetch('https://fakestoreapi.com/products')
+    //       .then((res) => {
+    //         if (!res.ok) {
+    //           throw new Error('API failed')
+    //         }
+    //         return res.json();
+    //       })
+    //       .then((res) => setData(res.map((product) => ({...product , count: 0}))))
+    //       .catch((error) => console.error('Error fetching data:', error))
+    //   }, [])
+
+
 
       const handleIncrease = (id) =>{
         const updateCount = data.map((product) => product.id === id ? {...product , count: product.count + 1} : product)  
         setData(updateCount)
-        setCartCount(cartCount + 1)
+        setTotalCount(totalCount + 1)
+
 
         
       }
@@ -37,7 +39,8 @@ function Product() {
       const handleDecrease = (id) =>{
         const updateCount = data.map((product) => product.id === id && product.count > 0 ? {...product , count : product.count - 1} : product)
         setData(updateCount)
-        setCartCount(cartCount - 1)
+        setTotalCount(totalCount - 1)
+
 
 
         
